4b4b72bcfdee074d6f4620cceda30ac7cce75ac9
Cezary Krawczyk <ckrawczyk@linux-p29-11.inf.ug.edu.pl>
2019-10-03 16:57:43 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int MAX_ITERATIONS = 10;
+        double e = 0.00;
+        double x = 1.00;
+        for(int i = 0; i < MAX_ITERATIONS; i++) {
+            e += Math.pow(x, i) / Math.
+        }
+    }
+
+}

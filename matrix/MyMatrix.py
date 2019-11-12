5699bf02d008bfae96775f7ef08f2e7b9d16fe96
Vengardd <33378090+Vengardd@users.noreply.github.com>
2019-11-12 11:29:36 +0100

    added MyMatrix class (#3)

---
+++ matrix/MyMatrix.py
@@ -0,0 +1,22 @@
+class MyMatrix:
+    matrix = [[]]
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def swapRows(self, x, y):
+        temp = self.matrix[x]
+        self.matrix[x] = self.matrix[y]
+        self.matrix[y] = temp
+
+    def swapColumns(self, x, y):
+        temp = []
+        for i in range(len(self.matrix)):
+            temp.append(self.matrix[i][x])
+        for i in range(len(self.matrix)):
+            self.matrix[i][x] = self.matrix[i][y]
+        for i in range(len(self.matrix)):
+            self.matrix[i][y] = temp[i]
+
+    def getValueFromPosition(self, x, y):
+       return self.matrix[x][y]

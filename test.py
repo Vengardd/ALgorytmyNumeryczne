4b6f2f217c3b09eb5a35347d9cf9ada1a3c91be6
mrybarczyk <tymphandorarose@gmail.com>
2019-11-12 21:06:23 +0100

    Wincyj zmian

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from number.FloatNumber import FloatNumber
 from number.NumberFabric import NumberFabric
 from number.NumberType import NumberType
+from matrix.MyMatrix import MyMatrix
 import numpy
 import random
 import fractions
@@ -10,7 +11,7 @@ import fractions
 
 # print("test")
 
-
+globalMax = 65536
 fabric = NumberFabric()
 numberType = NumberType.FLOAT
 
@@ -19,16 +20,25 @@ numberType = NumberType.FLOAT
 # print(number.value)
 
 def createMatrix(size, numberType):
-    mojaMacierz = [[None for i in range(size)] for j in range(size)]
+    a = [[None for i in range (0,size)] for j in range (0,size)]
+    x = [None for i in range (0,size)]
     for i in range (0,size):
+        nominator = random.uniform(-globalMax, globalMax - 1)
+        number = fabric.createNumberFromType(numberType, nominator, globalMax)
+        x[i] = number.value
         for j in range (0,size):
-            nominator = random.uniform(-500, 500)
-            denominator = random.uniform(-500, 500)
-            number = fabric.createNumberFromType(numberType, nominator, denominator)
-            mojaMacierz[i][j] = number.value
-    return mojaMacierz
+            nominator = random.uniform(-globalMax, globalMax-1)
+            number = fabric.createNumberFromType(numberType, nominator, globalMax)
+            a[i][j] = number.value
+    b = numpy.matmul(a,x)
+    return MyMatrix(a, b, x)
 
-print(createMatrix(10, numberType))
+m = createMatrix(10, numberType)
+size=10
+for i in range (0,size):
+    print()
+    for j in range (0,size):
+        print(m.getValueFromPosition(i, j))
 
 
 

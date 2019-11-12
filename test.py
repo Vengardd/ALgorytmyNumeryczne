d4b47a837061c6beff744028982e3fa93162b5af
mrybarczyk <tymphandorarose@gmail.com>
2019-11-12 20:33:46 +0100

    Zmiany w test.py

--- test.py
+++ test.py
@@ -1,12 +1,35 @@
 from number.FloatNumber import FloatNumber
 from number.NumberFabric import NumberFabric
 from number.NumberType import NumberType
+import numpy
+import random
+import fractions
 
-print("test")
+# ckrawczyk
+# mrybarczyk
+
+# print("test")
+
 
 fabric = NumberFabric()
 numberType = NumberType.FLOAT
-number = fabric.createNumberFromType(numberType, 5, 1)
-number.print()
-number.add(FloatNumber(10, 2))
-print(number.value)
+
+# number.print()
+# number.add(FloatNumber(10, 2))
+# print(number.value)
+
+def createMatrix(size, numberType):
+    mojaMacierz = [[None for i in range(size)] for j in range(size)]
+    for i in range (0,size):
+        for j in range (0,size):
+            nominator = random.uniform(-500, 500)
+            denominator = random.uniform(-500, 500)
+            number = fabric.createNumberFromType(numberType, nominator, denominator)
+            mojaMacierz[i][j] = number.value
+    return mojaMacierz
+
+print(createMatrix(10, numberType))
+
+
+
+

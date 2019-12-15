a11b0070b8170dcc70f9e69ca01658ac6271a9b9
Marta Rybarczyk <martarybarczyk98@gmail.com>
2019-12-15 16:55:34 +0100

    Zmienioni 1800 na 1100

--- implementation/Main.py
+++ implementation/Main.py
@@ -4,8 +4,8 @@ from implementation.test import Test
 
 filename50 = "amazon-meta-50.txt"
 filename300 = "amazon-meta-300.txt"
-filename1800 = "amazon-meta-1800.txt"
-names = [filename50, filename300, filename1800]
+filename1100 = "amazon-meta-1100.txt"
+names = [filename50, filename300, filename1100]
 d = [1, 2, 3, 4, 5]
 lamb = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 t = Test()
